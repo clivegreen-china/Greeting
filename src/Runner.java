2cbc7b87c479e92c1050ae31c13e89481a4ae3ea
Clive Green <17758440+clivegreen-china@users.noreply.github.com>
2021-04-09 19:07:00 +0800

    Update Runner.java
    Extended the greeting message.

--- src/Runner.java
+++ src/Runner.java
@@ -2,6 +2,6 @@ public class Runner {
 
     public static void main( String[] args ) {
 
-        System.out.print( "Greetings, Github!" );
+        System.out.print( "Greetings, Github! Good to be here." );
     }
 }
