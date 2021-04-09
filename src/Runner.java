b7c621f7e42962c187b3db47ab1d8ed0c02415a1
clivegreen <clivegreen@icloud.com>
2021-04-10 03:09:13 +0800

    Added a second message.

--- src/Runner.java
+++ src/Runner.java
@@ -3,5 +3,6 @@ public class Runner {
     public static void main( String[] args ) {
 
         System.out.print( "Greetings, Github! Good to be here." );
+        System.out.print( "Updating works both ways. Nice!" );
     }
 }
