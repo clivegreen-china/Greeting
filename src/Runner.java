0923f55fc4da0e4f5000b7a6ef4d03cc6c383689
clivegreen <clivegreen@icloud.com>
2021-05-31 05:37:13 +0800

    Removed the word Github.

--- src/Runner.java
+++ src/Runner.java
@@ -2,7 +2,7 @@ public class Runner {
 
     public static void main( String[] args ) {
 
-        System.out.print( "Greetings, Github! Good to be here." );
+        System.out.print( "Greetings! Good to be here." );
         System.out.print( "Updating works both ways. Nice!" );
     }
 }
